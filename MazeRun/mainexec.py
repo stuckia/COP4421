df3907f4ffcb1338b5240e6eec26596c7d76291d
Abby <70772021+stuckia@users.noreply.github.com>
2023-10-18 20:12:39 +0000

    Create mainexec.py

---
+++ MazeRun/mainexec.py
@@ -0,0 +1,190 @@
+import rclpy
+import random
+import math
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+
+class laserdrive(Node):
+
+  def __init__(self):
+    super().__init__('laserdrive')
+    self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
+    self.sub = self.create_subscription(LaserScan, 'scan', self.laser_callback, 10)
+    self.laserdata = None
+    self.minangle = 0
+    self.maxangle = 0
+    self.deltaangle = 0
+    self.maxrange = 0
+    self.timer = self.create_timer(0.1, self.move)
+
+  def laser_callback(self, msg):
+    self.laserdata = msg.ranges
+    self.minangle = msg.angle_min
+    self.maxangle = msg.angle_max
+    self.deltaangle = msg.angle_increment
+    self.maxrange = msg.range_max
+    for i in range(0, len(self.laserdata)):
+      if math.isinf(self.laserdata[i]):
+        self.laserdata[i] = self.maxrange
+
+  def getmindist(self, max_ang, min_ang):
+    min_index = self.angletoindex(min_ang * math.pi / 180)
+    max_index = self.angletoindex(max_ang * math.pi / 180)
+    return min(self.laserdata[min_index:max_index]) / (max_index - min_index)
+
+  def getmaxdist(self, max_ang, min_ang):
+    min_index = self.angletoindex(min_ang * math.pi / 180)
+    max_index = self.angletoindex(max_ang * math.pi / 180)
+    return max(self.laserdata[min_index:max_index]) / (max_index - min_index)
+
+  def getavgdist(self, max_ang, min_ang):
+    min_index = self.angletoindex(min_ang * math.pi / 180)
+    max_index = self.angletoindex(max_ang * math.pi / 180)
+    avg = 0
+    for i in range(min_index, max_index, 1):
+      avg += self.laserdata[i]
+    avg /= (max_index - min_index)
+    avg /= self.maxrange
+    return avg
+
+  def getrightsum(self):
+    rightsum = 0
+    count = 0
+    for i in range(self.angletoindex(-90 * math.pi / 180), self.angletoindex(-10 * math.pi / 180), 1):
+      rightsum += self.laserdata[i]
+      count += 1
+    rightsum /= count
+    rightsum /= self.maxrange
+    return rightsum
+
+  def getleftsum(self):
+    leftsum = 0
+    count = 0
+    for i in range(self.angletoindex(10 * math.pi / 180), self.angletoindex(90 * math.pi / 180), 1):
+      leftsum += self.laserdata[i]
+      count += 1
+    leftsum /= count
+    leftsum /= self.maxrange
+    return leftsum
+
+  def safewander(self):
+    #fwd movement
+    fwdsum = 1000
+    count = 0
+    for i in range(self.angletoindex(-10 * math.pi / 180),self.angletoindex(10 * math.pi / 180), 1):
+      if (fwdsum > self.laserdata[i]):
+        #min range in front arc
+        fwdsum = self.laserdata[i]
+        #avg arc -> fwdsum/=count
+        #normalize via max dist
+        fwdsum /= self.maxrange
+
+        rightsum = self.getrightsum()
+        leftsum = self.getleftsum()
+
+        msg = Twist()
+        msg.linear.x = fwdsum *2
+        msg.angular.z = (leftsum - rightsum) * math.pi * 2
+        self.pub.publish(msg)
+
+  def turn_right(self):
+    msg = Twist()
+    msg.linear.x = 0.7
+    msg.angular.z = 0.0
+    
+    while(self.getmindist(90,-90) != self.getmindist(-30,-90)):
+      msg.linear.x = 0.7
+      msg.angular.z = -0.8
+      self.pub.publish(msg)
+      
+  def turn_left(self):
+    msg = Twist()
+    msg.linear.x = 0.7
+    msg.angular.z = 0.0
+    
+    while(self.getmindist(90,-90) != self.getmindist(90,30)):
+      msg.linear.x = 0.7
+      msg.angular.z = 0.8
+      self.pub.publish(msg)
+
+  def wall_right(self):
+    desired_dist = 1
+    current_dist = self.getmindist(-30,-90)
+    diff = desired_dist - current_dist
+    msg = Twist()
+    msg.linear.x = 0.8
+    msg.angular.z = 0.0
+
+    if (current_dist < desired_dist):
+      msg.linear.x = 1.0
+      msg.angular.z = 0.5
+
+    self.pub.publish(msg)
+
+  def wall_left(self):
+    desired_dist = 1
+    current_dist = self.getmindist(90,30)
+    diff = desired_dist - current_dist
+    msg = Twist()
+    msg.linear.x = 1.0
+    msg.angular.z = 0.0
+
+    if (current_dist < desired_dist):
+      msg.linear.x = 1.0
+      msg.angular.z = 0.5
+
+    self.pub.publish(msg)
+
+  def move(self):
+    if self.laserdata is not None:
+      if (self.getmindist(90, -90) == self.getmindist(30, -30)):
+        #minimum is in front, pick either R or L wall follow
+        if (self.getrightsum() > self.getleftsum()):
+          self.turn_right()
+          self.wall_right()
+        elif (self.getleftsum() > self.getrightsum()):
+          self.turn_left()
+          self.wall_left()
+        else:
+          #fwd movement
+          fwdsum = 1000
+          count = 0
+          for i in range(self.angletoindex(-10 * math.pi / 180),self.angletoindex(10 * math.pi / 180), 1):
+            if (fwdsum > self.laserdata[i]):
+              #min range in front arc
+              fwdsum = self.laserdata[i]
+          #avg arc -> fwdsum/=count
+          #normalize via max dist
+          fwdsum /= self.maxrange
+
+          rightsum = self.getrightsum()
+          leftsum = self.getleftsum()
+
+          msg = Twist()
+          msg.linear.x = fwdsum
+          msg.angular.z = (leftsum - rightsum) * math.pi * 2
+          self.pub.publish(msg)
+      else:
+        #min is either on left or right
+        self.safewander()
+
+  def angletoindex(self, angle):
+    #in rad
+    return math.floor((angle - self.minangle) / self.deltaangle)
+
+  def indextoangle(self, index):
+    return (index * self.deltaangle) + self.minangle
+
+
+def main(args=None):
+  rclpy.init(args=args)
+  mover = laserdrive()
+  rclpy.spin(mover)
+  dp.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
